2f4a76a28a0a438a9646b1320a30eb915d804018
juanjomen <juanjomen@familysearch.org>
2021-03-18 11:54:25 -0600

    1

---
+++ src/test/java/com/example/onemore/OnemoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onemore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnemoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
